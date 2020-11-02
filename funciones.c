675a9e7f87739422704a9002ebfc4b712cb91c2f
Darkstrop <71039683+Darkstrop@users.noreply.github.com>
2020-11-02 00:00:39 -0300

    Update funciones.c
    organizacion de tipeo y reestructuracion de funciones

--- funciones.c
+++ funciones.c
@@ -29,59 +29,69 @@ char *get_txt_field (char * tmp, int k)
     return token;
 }
 
-int lower_than_string(void* key1, void* key2){
+int lower_than_string(void* key1, void* key2)
+{
     char* k1=(char*) key1;
     char* k2=(char*) key2;
     if(strcmp(k1,k2)<0) return 1;
     return 0;
 }
 
-int lower_than_int(void* key1, void* key2){ //This function compare 2 keys *int
+int lower_than_int(void* key1, void* key2) //This function compare 2 keys *int
+{ 
     int k1 = *((int*) (key1));
     int k2 = *((int*) (key2));
     return k1<k2;
 }
 
 
-void agregaInfo (HashMap* Ciudades, char* ciudad1, char* ciudad2, int distancia){
-
+void agregaInfo (HashMap* Ciudades, char* ciudad1, char* ciudad2, int distancia)
+{
 Ciudad * city1;
- Ciudad * city2;
+Ciudad * city2;
 
-      if (searchMap(Ciudades,ciudad1)== NULL){
+      if (searchMap(Ciudades,ciudad1)== NULL)
+      {
        city1 = crearCiudad(ciudad1);
-       insertMap(Ciudades,ciudad1,city1);}
-      else {
+       insertMap(Ciudades,ciudad1,city1);
+      }
+      else 
+      {
        city1 = searchMap(Ciudades,ciudad1);
       }
 
-     if (searchMap(Ciudades,ciudad2)== NULL){
+     if (searchMap(Ciudades,ciudad2)== NULL)
+     {
         city2 = crearCiudad(ciudad2);
-        insertMap(Ciudades, ciudad2,city2);}
-      else {
+        insertMap(Ciudades, ciudad2,city2);
+     }
+      else 
+      {
        city2 = searchMap(Ciudades,ciudad2);
       }
 
-     if(searchMap(city1->distancias, ciudad2) == NULL){
+     if(searchMap(city1->distancias, ciudad2) == NULL)
+     {
         insertMap(city1->distancias,ciudad2,distancia);
      }
 
-     if(searchMap(city2->distancias, ciudad1) == NULL){
+     if(searchMap(city2->distancias, ciudad1) == NULL)
+     {
         insertMap(city2->distancias,ciudad1,distancia);
      }
-
 }
 
 
-Ciudad * crearCiudad(char * name ) {
+Ciudad * crearCiudad(char * name ) 
+{
    Ciudad* nuevaCiudad= (Ciudad *) malloc (sizeof(Ciudad));
    strcpy( nuevaCiudad->nombre,name);
-  nuevaCiudad->distancias = createMap(200);
+   nuevaCiudad->distancias = createMap(200);
  return nuevaCiudad;
 }
 
-char * toString(int id){
-
+char * toString(int id)
+{
  //function that convert a string to a int
     int digitos = 0;
     int aux = id;
@@ -92,55 +102,51 @@ char * toString(int id){
 
         aux = aux / 10;
         digitos++;
-
-    }
+}
 
     aux = id;
     auxDigitos = digitos;
     digitos--;
-
     char * idString = (char *) malloc ( (auxDigitos + 1) * sizeof(char));
-
-    while(aux != 0){
-
+    while(aux != 0)
+    {
         numAlreves = numAlreves + ( (aux % 10) * pow(10, digitos) );
         aux = aux / 10;
         digitos--;
-
     }
 
     aux = id;
     digitos = auxDigitos;
     int i = 0;
 
-    while(numAlreves > 0){
-
+    while(numAlreves > 0)
+    {
         idString[i] = (numAlreves%10) + 48;
         i++;
         numAlreves = numAlreves / 10;
-
     }
-
+    
     idString[i] = '\0';
-
     return idString;
-
 }
 
 
-void ImportarCiudades( HashMap * Ciudades){
+void ImportarCiudades( HashMap * Ciudades)
+{
     system("cls");
     printf("Please enter the file name (n_n) ");
     char name[50]; //the name of the file that has the games
     FILE * fp;
     Pair * check;
 
-    do{
+    do
+    {
 
         fflush(stdin);
         scanf("%[^\n]s", name);
         fp = fopen( name , "r"); //opens the file in read
-        if (fp == NULL){
+        if (fp == NULL)
+        {
             printf("Invalid file :( Please enter a valid file! "); //if the file isn't valid this message will pop on the screen
         }
         fflush(stdin);
@@ -148,28 +154,23 @@ void ImportarCiudades( HashMap * Ciudades){
     }while(fp == NULL);
 
     char line [1024];
-
-
-    while( fgets(line, 1023, fp) != NULL ){  //read and get every field of the csv file
+    while( fgets(line, 1023, fp) != NULL ) //read and get every field of the csv file
+    {  
 
         char * ciudad1 = get_txt_field(line, 0);
         char * ciudad2 = get_txt_field(line, 1);
         int distancia =  atoi(get_txt_field(line, 2));
         agregaInfo(Ciudades,ciudad1,ciudad2,distancia);
-
     }
-
-
+    
     printf("All the Cities were imported correctly (^_^) "); //message that pops out on the window if all the games were imported correctly
     system("pause");
     system("cls");
-
-
-
 }
 
 
-void  ciudadMasCercana (HashMap * Ciudades, char * nombreCiudad){
+void  ciudadMasCercana (HashMap * Ciudades, char * nombreCiudad)
+{
 
 Ciudad * ciudad;
 ciudad = searchMap(Ciudades,nombreCiudad);
